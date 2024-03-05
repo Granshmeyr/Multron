58d470256a1007f5fc1d639a60f02eff7586d870
Granshmeyr <playstationmelvin@gmail.com>
2024-03-04 21:33:46 -0500

    handlebar working

--- src/renderer/src/components/TileApp.tsx
+++ src/renderer/src/components/TileApp.tsx
@@ -1,69 +1,70 @@
-import {ReactElement, useState} from "react";
+import React, {ReactElement, useState} from "react";
 import {Direction} from "../../../common/enums";
 
 let listenerRegistered: boolean = false;
 
 export function TileApp(): ReactElement {
-  return <div className="flex w-screen h-screen">
-    <Tile />
-  </div>;
+  return (
+    <div className="flex w-screen h-screen">
+      <Tile className="grow"/>
+    </div>
+  );
 }
 
 function RowTile(
   {children}: {children: [ReactElement, ReactElement]}
 ): ReactElement {
-  return <div className="flex flex-row flex-grow w-full h-full">
-    {children}
-  </div>;
-}
-
-function ColumnTile(
-  {children}: {children: [ReactElement, ReactElement]}
-): ReactElement {
-  return <div
-    className="flex flex-col w-full h-full"
-    style={{ flex: "5" }}>
-    {children}
-  </div>;
-}
-
-function ColoredBar({color, width, height}: {color: string; width: string; height: string}): ReactElement {
   return (
-    <div
-      style={{
-        backgroundColor: color,
-        width: width,
-        height: height,
-      }}
-    />
+    <div className="flex grow flex-row basis-1/2">
+      {children}
+    </div>
   );
 }
 
-function OverlayCenter(
-  {children}: {children: [ReactElement, ReactElement]}
+function ColumnTile(): ReactElement {
+  const [verticalPercent, setVerticalPercent] = useState(0.5);
+
+  const onDrag = (e: React.DragEvent<HTMLDivElement>) => {
+    setVerticalPercent(e.clientY / window.innerHeight);
+  };
+
+  const grow1: string = `${verticalPercent}`;
+  const grow2: string = `${1 - verticalPercent}`;
+  console.log(grow1);
+  console.log(grow2);
+
+  return (
+    <div className="flex grow flex-col">
+      <Tile style={{flexGrow: grow1}} />
+      <Handlebar onDrag={onDrag} />
+      <Tile style={{flexGrow: grow2}} />
+    </div>
+  );
+}
+
+function Handlebar(
+  { onDrag }: {
+  onDrag: (e: React.DragEvent<HTMLDivElement>) => void;
+  }
 ): ReactElement {
   return (
     <div
-      className="relative flex items-center justify-center"
-      style={{flex: "5"}}>
-      {children[0]}
-      <div
-        className="absolute flex top-0 left-0 w-full h-full items-center justify-center"
-        style={{pointerEvents: "none"}}>
-        {children[1]}
-      </div>
+      className="h-0 relative"
+      onDrag={onDrag}
+    >
+      <div className="handlebar" />
     </div>
   );
 }
 
 function Tile(
-  {className}: {
-    className?: string,
-    key?: string | number
+  {className, style}: {
+    className?: string
+    style?: React.CSSProperties
  }
 ): ReactElement {
   const [splitDirection, setSplitDirection] = useState<Direction | null>(null);
-  const defaultClass: string = "flex border-4 border-white border-opacity-50";
+  const defaultClass: string = "border-4 border-white border-opacity-50";
 
   async function showSplitMenu(): Promise<Direction | null> {
     return new Promise<Direction | null>((resolve) => {
@@ -81,35 +82,45 @@ function Tile(
   }
 
   function tileElement(): ReactElement {
-    return <div
-      className={`${defaultClass} ${className}`}
-      onContextMenu={
-        async () => {
-          const direction: Direction | null = await showSplitMenu();
-          if (direction == null) {
-            return;
+    return (
+      <div
+        className={`${defaultClass} ${className}`}
+        onContextMenu={
+          async () => {
+            const direction: Direction | null = await showSplitMenu();
+            if (direction == null) {
+              return;
+            }
+            setSplitDirection(direction);
           }
-          setSplitDirection(direction);
         }
-      }
-      style={{flex: "5"}}>
-    </div>;
+        style={style}
+      >
+      </div>
+    );
   }
 
   function splitLogic(): ReactElement {
     const element: ReactElement = (() => {
       switch (splitDirection) {
       case Direction.Up:
-        return <OverlayCenter>
-          <ColumnTile><Tile /><Tile /></ColumnTile>
-          <ColoredBar color="blue" width="50px" height="5px" />
-        </OverlayCenter>;
+        return <ColumnTile />;
       case Direction.Down:
-        return <ColumnTile><Tile /><Tile /></ColumnTile>;
+        return <ColumnTile />;
       case Direction.Left:
-        return <RowTile><Tile /><Tile/></RowTile>;
+        return (
+          <RowTile>
+            <Tile />
+            <Tile />
+          </RowTile>
+        );
       case Direction.Right:
-        return <RowTile><Tile /><Tile /></RowTile>;
+        return (
+          <RowTile>
+            <Tile />
+            <Tile />
+          </RowTile>
+        );
       default:
         return <></>;
       }
